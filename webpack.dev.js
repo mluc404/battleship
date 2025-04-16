394b10c3899e2f4ec3ce28c99b6099a4c6736afc
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-04-16 15:19:11 -0700

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const config = require("./webpack.config.js");
+
+module.exports = merge(config, {
+  mode: "development",
+
+  devtool: "eval-source-map",
+
+  devServer: {
+    watchFiles: ["src/**/*", "dist/*.html"],
+    static: "./dist",
+    open: true,
+
+    compress: true,
+    historyApiFallback: true,
+
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: "all",
+
+    hot: true,
+    liveReload: true,
+  },
+});
